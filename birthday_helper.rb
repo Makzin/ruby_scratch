9dc061b5ea53436564474c1886e49c7df318ef8f
Max Van Lyl <maximvanlyl@gmail.com>
2017-03-08 18:32:10 -0500

    Added extra functionality to birthday_helper

--- birthday_helper.rb
+++ birthday_helper.rb
@@ -12,10 +12,14 @@ end
 puts "Whose birth date would you like to know?"
 chosen_one = gets.chomp
 bday = birth_dates[chosen_one]
+date, year = bday.split(',')
 
+now = Time.new
+age = now.year - year.to_i
 
 if bday == nil
   puts 'ooh, I don\'t know that one...!'
 else
   puts "#{chosen_one}'s birtday is on #{bday}"
+  puts "They will be turning #{age} on that day."
 end
